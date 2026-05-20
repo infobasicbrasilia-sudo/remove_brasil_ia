48a63e818736c72c3c3615ecf28d0cca865423d0
infobasicbrasilia-sudo <infobasicbrasilia@gmail.com>
2026-05-20 17:45:29 -0300

    atualizacao

--- api/counter.js
+++ api/counter.js
@@ -11,51 +11,20 @@ export default async function handler(req, res) {
             "counter.json"
         );
 
-        // cria arquivo se não existir
-        if (!fs.existsSync(filePath)) {
-
-            fs.writeFileSync(
-                filePath,
-                JSON.stringify({
-                    visits: 504
-                }, null, 2)
-            );
-        }
-
-        // lê arquivo
-        const rawData = fs.readFileSync(
-            filePath,
-            "utf8"
+        const data = JSON.parse(
+            fs.readFileSync(filePath, "utf8")
         );
 
-        const data = JSON.parse(rawData);
-
-        // garante número válido
-        if (typeof data.visits !== "number") {
-            data.visits = 504;
-        }
-
-        // soma acesso
-        data.visits += 1;
-
-        // salva
-        fs.writeFileSync(
-            filePath,
-            JSON.stringify(data, null, 2)
-        );
-
-        // retorna json
         return res.status(200).json({
             visits: data.visits
         });
 
     } catch (error) {
 
-        console.error("ERRO COUNTER:", error);
+        console.error(error);
 
         return res.status(500).json({
-            visits: 0,
-            error: "Erro contador"
+            visits: 0
         });
     }
 }
